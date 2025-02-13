45cd6f1e43126fb5d35b035900ac02c8c09ab42a
NatthaphongS <sinsupharoek@gmail.com>
2025-02-13 08:47:27 +0700

    set env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return process.env.OPEN_AI_API_KEY || 'not found';
   }
 }
